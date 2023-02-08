6aff78fb1dfdf4789b1d120aaa831e46d1a346bc
Artur Prakapenka <arturprokopenko01@gmail.com>
2023-02-08 13:33:47 +0300

    first iteration is completed

--- src/gui/views/main_screen/main_screen.py
+++ src/gui/views/main_screen/main_screen.py
@@ -1,9 +1,13 @@
+from dataclasses import astuple
 from typing import List
 
 import asynckivy as ak
 from kivy.factory import Factory
+from kivy.metrics import dp
 from kivy.properties import ObjectProperty
 from kivy.uix.modalview import ModalView
+from kivymd.uix.card import MDCard
+from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.filemanager import MDFileManager
 from kivymd.uix.screen import MDScreen
 
@@ -26,6 +30,28 @@ class MainScreenView(MDScreen):
         self._file_manager_opening_mode = FileManagerOpeningMods.IMPORT
         self._success_import_dialog = self.__init_success_import_dialog()
         self._success_export_dialog = self.__init_success_export_dialog()
+        self._workload_datatable_dialog = self.__init_workload_datatable_dialog()
+
+    @staticmethod
+    def __init_workload_datatable_dialog():
+        dialog = ModalView(size_hint=(None, None), size=(600, 420))
+        container = MDCard()
+        content = MDDataTable(
+            use_pagination=True,
+            check=True,
+            column_data=[
+                ("Mentor", dp(60)),
+                ("Group", dp(30)),
+                ("Subject", dp(60)),
+                ("Subject Type", dp(30)),
+                ("Hours", dp(20)),
+            ],
+            elevation=2,
+        )
+        container.add_widget(content)
+        dialog.add_widget(container)
+        dialog.table = content
+        return dialog
 
     @staticmethod
     def __init_success_import_dialog():
@@ -66,8 +92,15 @@ class MainScreenView(MDScreen):
     def exit_file_manager(self, *args):
         self._file_manager.close()
 
+    def open_workload_data_table_dialog(self, *args):
+        self._workload_datatable_dialog.open()
+
     async def update_workload(self, mentor_load_items: List[MentorLoadItem], show_message=False):
         self.configuration_description_screen.update_workload(mentor_load_items)
+        self._workload_datatable_dialog.table.update_row_data(
+            instance_data_table=self._workload_datatable_dialog.table,
+            data=[astuple(item) for item in mentor_load_items]
+        )
         if show_message:
             self._success_import_dialog.open()
 
