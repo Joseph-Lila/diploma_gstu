8e3b9d6b1733b0362c985db7ceb92f51d7a7c0e8
Artur Prakapenka <arturprokopenko01@gmail.com>
2023-04-12 11:01:50 +0300

    mentors schedule is completed

--- src/ui/views/mentors_schedule/mentors_schedule.py
+++ src/ui/views/mentors_schedule/mentors_schedule.py
@@ -1,5 +1,11 @@
 from kivymd.uix.card import MDCard
 
+from src import config
+
 
 class MentorsSchedule(MDCard):
-    pass
+    def fill_grid_with_records(self, records, pairs_time=config.get_pairs_time()):
+        # create pairs time block
+
+        # create other blocks for each mentor
+        raise NotImplementedError
